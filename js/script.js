4929b7189ba84ed8423fb488d3f3908ce0bfca6c
Crescenzo De Martino <cresckamina@gmail.com>
2020-07-07 00:01:27 +0200

    Milestone 5 - Aggiunti generi e cast e piccoli bugfix

--- js/script.js
+++ js/script.js
@@ -52,8 +52,9 @@ $(document).ready(function() {
 
   // Al click di un titolo mostro il collegamento alla pagina di TMDB
   $(document).on('click','.object',function () {
+    var type = $(this).attr('typeObject')
     var id = $(this).attr('idObject')
-    window.open('https://www.themoviedb.org/' + id + '?language=it-IT');
+    window.open('https://www.themoviedb.org/' + type + '/' + id + '/' + '?language=it-IT');
   })
 
   function ajaxCall(valQuery) {
@@ -130,14 +131,88 @@ $(document).ready(function() {
           vote_average: star(singleObject.vote_average),
           langFlag: langFlag(singleObject.original_language),
           overview: singleObject.overview,
-          id: type + '/' + singleObject.id
+          type: type,
+          id: singleObject.id
         }
         var html = template(context);
         container.append(html)
+
+        getCast(type,singleObject.id)
       }
     }
   }
 
+  // Funzione che esegue chiamata AJAX per reperire dati "credits"
+  function getCast(type,id){
+    $.ajax(
+      {
+        url: 'https://api.themoviedb.org/3/'+ type + '/' + id,
+        method: 'GET',
+        data: {
+          api_key: '963cb474d673e5002daba73431170dc1',
+          language: 'it-IT',
+          append_to_response: 'credits'
+        },
+        success: function(dataObject) {
+          printInfo(dataObject,id)
+        },
+        error: function () {
+          alert('Attenzione si è verificato un errore')
+        }
+      }
+    )
+  }
+
+  // Funzione che stampa i risultati prelevando quelli che servono dalla precedente chiamata AJAX
+  function printInfo(data,id){
+  var object = $('.object[idObject="'+ id +'"]');
+  // Creo lista generi singolo film
+  var genres = data.genres;
+  var arrayGenres = [];
+  // Se ci sono uno o più generi li mostro in fila
+  if (genres.length > 0) {
+    for (var i = 0; i < genres.length; i++) {
+      arrayGenres += '&bull; ' + genres[i].name + '  ';
+    }
+  // In caso non siano disponibili generi, mostro la scritta "not available"
+  } else {
+    arrayGenres += 'Not Available'
+  }
+
+
+  // Creo lista primi 5 attori
+  var castActors = data.credits.cast;
+  var arrayActors = [];
+  // Se sono tra 1 e 4 attori ne stampo la stessa quantità
+  if (castActors.length > 0 && castActors.length < 5) {
+    for (var i = 0; i < castActors.length; i++) {
+      arrayActors += '- ' + castActors[i].name + '<br>';
+    }
+  // Se sono più di 5 attori ne stampo solo 5
+  } else if (castActors.length >= 5) {
+    for (var i = 0; i < 5; i++) {
+      arrayActors += '- ' + castActors[i].name + '<br>';
+    }
+  // Se la lista attori è uguale a 0 mostro la dicitura "not available"
+  } else {
+    arrayActors += 'Not Available'
+  }
+
+  // Imposto Handlebars
+  var source = $('#genres-actors-template').html()
+  var template = Handlebars.compile(source);
+
+  // Imposto il context
+  var context = {
+    genres: arrayGenres,
+    actors: arrayActors
+  };
+  var html = template(context);
+  // Appendo il risultato creando altri due list item nella ul pre esistente
+  $(object).find('ul').append(html);
+}
+
+  // Funzione che crea le stelle di recensione
   function star(vote) {
     var voteApprox = Math.ceil((vote/2))
     var starVote = ''
@@ -151,6 +226,7 @@ $(document).ready(function() {
     return starVote
   }
 
+  // Funzione che imposta il path delle copertine a seconda se siano disponibili o meno
   function posterPath(path) {
     var finalPath = 'img/noPoster.jpg'
     if (path != null) {
@@ -159,9 +235,10 @@ $(document).ready(function() {
     return finalPath
   }
 
+  // Funzione che crea le bandiere a seconda della lingua
   function langFlag(language) {
     var langArray = ['en','us','it','fr','de','es','pt','ja','ru','ro',
-    'zh','ko','tr','id','cs','hi','sv','he']
+    'zh','ko','tr','id','cs','hi','sv','he','nl']
     var langFlagPath = 'img/noFlag.png'
     if (langArray.includes(language)) {
       langFlagPath = 'img/'+language+'.png'
